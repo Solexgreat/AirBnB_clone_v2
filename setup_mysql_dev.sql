ec21d57a2a71c8ca11da19fb72e2682e598b1921
Solexgreat <Solomongodkid>
2022-11-16 19:29:22 -0800

    updated files

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-CREATE DATABASE [IF NOT EXISTS] hbnb_dev_db
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
-=======
--- prepares a MySQL server for the project
-
+-- Creating hbnb_dev_db database
+-- Privileges for new user (hbnb_dev)
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
->>>>>>> 970421cd2016e99fcb6f5b4801805059dbe4f08e
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+USE hbnb_dev_db;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
